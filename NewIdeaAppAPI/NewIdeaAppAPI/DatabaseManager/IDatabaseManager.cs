8ebc1461c00fab4452186c8d14a9ab1a3fcf48e4
anthony-elhaddad <37693794+anthony-elhaddad@users.noreply.github.com>
2019-12-20 11:09:44 +0200

    Add files via upload

---
+++ NewIdeaAppAPI/NewIdeaAppAPI/DatabaseManager/IDatabaseManager.cs
@@ -0,0 +1,17 @@
+﻿using NewIdeaAppAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewIdeaAppAPI.DatabaseManager
+{
+    public interface IDatabaseManager<T> where T : BaseEntity
+    {
+        void Add(T item);
+        void Remove(int id);
+        void Update(T item);
+        T FindByID(int id);
+        IEnumerable<T> FindAll();
+    }
+}
